35a3be82ec7fb727a519765037974872dd28353b
Andrew Brennwald <Ando@CloudCity.local>
2017-02-08 20:38:40 -0800

    Implement experimental event handler for subject header.

--- src/Subject.js
+++ src/Subject.js
@@ -3,14 +3,29 @@ import React, { Component } from 'react';
 export default class Subject extends Component {
   constructor(props) {
     super(props)
+
+    this.handleClick = this.handleClick.bind(this);
+
+    this.state = {
+      isClicked: false
+    }
   }
 
-  // event handlers
+  handleClick() {
+      this.setState(prevState => ({
+        isClicked: !prevState.isClicked
+      }));
+      console.log(this.state.isClicked);
+
+      setTimeout(() => {
+        console.log(this.state.isClicked)
+      }, 100)
+  }
 
   render() {
     return(
       <div>
-        <h2>{this.props.items.subject}</h2>
+        <h2 onClick={this.handleClick}>{this.props.items.subject}</h2>
 
         <ul>
           {
